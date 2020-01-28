31415a16ae2084026dad6002dfb4abd86daf7272
Patrick <patrick.lopdrup@gmail.com>
2020-01-28 14:58:53 +0100

    added the welcome screen with first name shown. changed all context!!.getString to resources.getString

--- app/src/main/java/com/example/orstedwidget/LoadingFrag.kt
+++ app/src/main/java/com/example/orstedwidget/LoadingFrag.kt
@@ -1,6 +1,7 @@
 package com.example.orstedwidget
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -29,16 +30,16 @@ class LoadingFrag : Fragment() {
         val layout = inflater.inflate(R.layout.frag_loading_log_in, container, false)
 
         val sharedPref = context!!.getSharedPreferences(
-            context!!.getString(R.string.sharedPrefs_log_in_key),
+            resources.getString(R.string.sharedPrefs_log_in_key),
             Context.MODE_PRIVATE
         )
         email = sharedPref.getString(
-            context!!.getString(R.string.email_key),
-            context!!.getString(R.string.empty_text)
+            resources.getString(R.string.email_key),
+            resources.getString(R.string.empty_text)
         )!!
         password = sharedPref.getString(
-            context!!.getString(R.string.password_key),
-            context!!.getString(R.string.empty_text)
+            resources.getString(R.string.password_key),
+            resources.getString(R.string.empty_text)
         )!!
 
         CoroutineScope(Main).launch {
@@ -49,9 +50,20 @@ class LoadingFrag : Fragment() {
                 firstName = result.getValue("first_name")
                 println("firstname: $firstName")
 
+                //adding first name to shared prefs
+                val editor = sharedPref.edit()
+                editor.putString(resources.getString(R.string.first_name_key), firstName)
+                editor.apply()
+
+                //
+                fragmentManager!!.beginTransaction()
+                    .replace(R.id.main_frameLayout, WelcomeFrag())
+                    .commit()
+
+            //when we get a server error
             } catch (e: FileNotFoundException) {
-                println(context!!.getString(R.string.user_not_found))
-                Toast.makeText(context, context!!.getString(R.string.user_not_found), Toast.LENGTH_LONG).show()
+                println(resources.getString(R.string.user_not_found))
+                Toast.makeText(context, resources.getString(R.string.user_not_found), Toast.LENGTH_LONG).show()
                 fragmentManager!!.beginTransaction()
                     .replace(R.id.main_frameLayout, LogInFrag())
                     .commit()
